266893323759236e60412893c3f352a86b1df27f
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-05-11 23:32:11 +0200

    Add JSX CardForm add dispatch

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,14 +1,20 @@
 import styles from './CardForm.module.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
 const CardForm = (props) => {
   const [title, setTitle] = useState('');
 
+  const dispatch = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.action({ title: title }, props.columnId);
+    dispatch({
+      type: 'ADD_CARD',
+      payload: { title: title, columnId: props.columnId },
+    });
     setTitle('');
   };
 
